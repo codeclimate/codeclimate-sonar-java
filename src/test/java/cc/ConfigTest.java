e01597a59a0a491e3cfa9f76a69b358f19ecf3f6
Filipe Esperandio <filipesperandio@gmail.com>
2017-10-05 14:40:52 -0300

    Add initial support to config.json

---
+++ src/test/java/cc/ConfigTest.java
@@ -0,0 +1,15 @@
+package cc;
+
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void include_paths() throws Exception {
+        Config config = Config.from("fixtures/multiple_paths/config.json");
+        assertThat(config.includePaths).containsOnly("Main.java", "src/main/java/");
+    }
+}
