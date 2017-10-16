b1eab41e035850f1620bd72cb3ff4e3366cd1aec
Filipe Esperandio <filipesperandio@gmail.com>
2017-10-16 14:43:11 -0200

    Remove rule type filter

--- src/main/java/cc/report/JsonReport.java
+++ src/main/java/cc/report/JsonReport.java
@@ -8,18 +8,11 @@ import org.sonarsource.sonarlint.core.client.api.common.analysis.AnalysisResults
 import org.sonarsource.sonarlint.core.client.api.common.analysis.Issue;
 import org.sonarsource.sonarlint.core.tracking.Trackable;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 import java.util.function.Function;
 
 public class JsonReport implements org.sonarlint.cli.report.Reporter {
-
-    private static final List<String> VALID_RULE_DETAIL_TYPES = Arrays.asList(
-            "BUG",
-            "VULNERABILITY"
-    );
 
     final Gson gson;
     final String baseDir;
@@ -29,18 +22,14 @@ public class JsonReport implements org.sonarlint.cli.report.Reporter {
         this.gson = new GsonFactory().create();
     }
 
-
     @Override
     public void execute(String projectName, Date date, Collection<Trackable> trackables, AnalysisResults result, Function<String, RuleDetails> ruleDescriptionProducer) {
         for (Trackable trackable : trackables) {
             Issue issue = trackable.getIssue();
             RuleDetails ruleDetails = ruleDescriptionProducer.apply(issue.getRuleKey());
 
-            String type = ruleDetails.getType();
-            if (VALID_RULE_DETAIL_TYPES.contains(type)) {
-                CodeClimateIssue codeClimateIssue = CodeClimateIssue.from(issue, ruleDetails, baseDir);
-                System.out.println(gson.toJson(codeClimateIssue) + "\0");
-            }
+            CodeClimateIssue codeClimateIssue = CodeClimateIssue.from(issue, ruleDetails, baseDir);
+            System.out.println(gson.toJson(codeClimateIssue) + "\0");
         }
     }
 }
