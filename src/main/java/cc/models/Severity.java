80a1a7291beb8efcf993182b6d96e6412907f864
Filipe Esperandio <filipesperandio@gmail.com>
2017-10-19 16:49:37 -0200

    Convert Severity into Enum

--- src/main/java/cc/models/Severity.java
+++ src/main/java/cc/models/Severity.java
@@ -3,13 +3,23 @@ package cc.models;
 import com.google.gson.annotations.SerializedName;
 import org.sonarsource.sonarlint.core.client.api.common.RuleDetails;
 
-public class Severity {
+public enum Severity {
+    @SerializedName("info")
+    INFO,
+    @SerializedName("minor")
+    MINOR,
+    @SerializedName("major")
+    MAJOR,
+    @SerializedName("critical")
+    CRITICAL,
+    @SerializedName("blocker")
+    BLOCKER;
 
-    public static String from(RuleDetails ruleDetails) {
+    public static Severity from(RuleDetails ruleDetails) {
         String severity = ruleDetails.getSeverity();
-        if(severity == null) {
+        if (severity == null) {
             return null;
         }
-        return severity.toLowerCase();
+        return valueOf(severity.toUpperCase());
     }
 }
