1d2987cdbff633fe482687ebb438649738fbe8af
Filipe Esperandio <filipesperandio@gmail.com>
2017-11-02 17:17:58 -0200

    Use sonar-wrapper lib (#50)

--- fixtures/multiple_paths/Main.java
+++
@@ -1,9 +0,0 @@
-import pkg1;
-
-public class Main {
-    public void main(String[] args) {
-        for (int k = 0; k < 20; i++) {  // cause issue
-            System.out.println(new Class1());
-        }
-    }
-}
