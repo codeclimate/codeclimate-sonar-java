71ff2844edabd40cc205b100f86fcfacfdf83109
Federico Moya <federicomoyamartin@gmail.com>
2020-05-15 13:57:04 -0300

    Add test for Java 11 source version

--- src/test/java/integration/JavaSourceVersion.java
+++ src/test/java/integration/JavaSourceVersion.java
@@ -11,4 +11,10 @@ public class JavaSourceVersion {
         Shell.Process process = Shell.execute("build/codeclimate-sonar fixtures/java_source_version fixtures/java_source_version/config.json");
         assertThat(process.stderr).contains("Configured Java source version (sonar.java.source): 6");
     }
+
+    @Test
+    public void specify_java_11_source_version_through_config() throws Exception {
+        Shell.Process process = Shell.execute("build/codeclimate-sonar fixtures/java_source_version fixtures/java_source_version/config_11.json");
+        assertThat(process.stderr).contains("Configured Java source version (sonar.java.source): 11");
+    }
 }
